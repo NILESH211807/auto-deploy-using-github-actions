4ebe1769a13e81b3aebbd38c96d8324fcf985257
NILESH PASWAN <nileshkumar0815@gmail.com>
2026-02-02 16:38:34 +0530

    app changes

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!! v1.0.0");
+  res.json({
+    message: "Hello World!!",
+    status: "success",
+    version: "1.0.0",
+  });
 });
 
 app.listen(port, () => {
